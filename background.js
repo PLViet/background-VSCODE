841b47b2866db61b90790470d47aa86e3c030e3c
Việt Lê <43601619+PLViet@users.noreply.github.com>
2021-11-25 10:08:07 +0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,46 @@
+// Background:
+       
+           "background.enabled": true,
+           "background.loop": false,
+           "background.useDefault": false,
+           "background.useFront": false,
+           "background.style": {
+               
+                 "content": "''",
+                 "pointer-events": "none",
+                 "position": "absolute",
+                 "z-index": "99999",
+                 "width": "100%",
+                 "height": "100%",
+                 "margin-left":"0%",
+                 "background-position": "right",
+                 "background-size": "cover",
+                 "background-repeat": "no-repeat",
+                 "opacity": 0.2
+           },
+           "background.customImages": [
+           
+
+               
+       
+               "https://msmobile.com.vn/upload_images/images/hinh-nen-dep-cho-may-tinh-full-hd-2.jpg",
+             //  "C:/Users/Việt Lê/Pictures/Saved Pictures/anhnen.jpg"
+           ]
+
+
+
+    // Animation:
+    "editor.cursorSmoothCaretAnimation": true,
+    "window.zoomLevel": 1,
+    "workbench.iconTheme": "vscode-icons",
+    "editor.fontFamily": "'Fira Code', Consolas, 'Courier New', monospace",
+    "terminal.integrated.fontFamily": "'Fira Code', Consolas, 'Courier New', monospace",
+    "editor.fontLgatures": true,
+    "editor.suggestSelection": "first",
+    "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue",
+    "tabnine.experimentalAutoImports": true,
+    "workbench.colorTheme": "Theme + Dark Blue Theme",
+    "terminal.integrated.fontSize": 12,
+    "C_Cpp.updateChannel": "Insider",
+    
+}
